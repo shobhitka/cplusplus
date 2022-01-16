12f54aaf843dce6c5ea6b0443517a95b4fcdd682
Shobhit Kumar <kumar@shobhit.info>
2022-01-16 13:32:47 +0530

    Add simple menu to work in the book store
    Signed-off-by: Shobhit Kumar <kumar@shobhit.info>

--- bookstore/src/main.cpp
+++ bookstore/src/main.cpp
@@ -13,6 +13,16 @@ void signal_hanlde(int signum)
 	exit(0);
 }
 
+void menu()
+{
+	cout << "-----------------------------------------" << endl;
+	cout << "1. Add new Book" << endl;
+	cout << "2. Display all book details" << endl;
+	cout << "3. Search book" << endl;
+	cout << "4. Exit" << endl;
+	cout << "-----------------------------------------" << endl;
+}
+
 int main()
 {
 	char title[32];
@@ -29,11 +39,33 @@ int main()
 	store->addBook("Java", 200.00, 20);
 
 	while (1) {
-		cout << "Enter title to search: ";
-		cin >> title;
-		cout << "Enter needed quantities: ";
-		cin >> quantity;
+		int choice;
+		menu();
+		cout << "Enter choise: ";
+		cin >> choice;
 
-		store->searchBook(title, quantity);
+		switch (choice) {
+			case 1:
+				store->addBook();
+				break;
+			case 2:
+				store->displayBooks();
+				break;
+			case 3:
+				cout << "Enter title to search: ";
+				cin >> title;
+				cout << "Enter needed quantities: ";
+				cin >> quantity;
+				store->searchBook(title, quantity);
+				break;
+			case 4:
+				cout << "Thank you. Bye." << endl;
+				BookStore::deleteInstance();
+				exit(0);
+			default:
+				cout << "Invalid choice" << endl;
+				menu();
+				break;
+		};
 	}
 }
